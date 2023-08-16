e09757cb129e4e3901eb8562cf6dc3a45b835b46
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-17 00:58:26 +0200

    tools added

--- backend/tools.js
+++ backend/tools.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import * as tools from './tools.js';
 
 export const createFile = (pathAndFileName, content) => {
 	fs.writeFileSync(pathAndFileName, content.trim());
@@ -6,7 +7,7 @@ export const createFile = (pathAndFileName, content) => {
 
 export const addLineInFile = (pathAndFileName, marker, additionalLine) => {
 	const content = fs.readFileSync(pathAndFileName, { encoding: 'utf8' });
-	const lines = qstr.convertStringBlockToLines(content);
+	const lines = tools.convertStringBlockToLines(content);
 	let newLines = [];
 	let lineNumber = 1;
 	for (const line of lines) {
@@ -20,6 +21,66 @@ export const addLineInFile = (pathAndFileName, marker, additionalLine) => {
 		}
 		lineNumber++;
 	}
-	const newContent = qstr.convertLinesToStringBlock(newLines);
-	qfil.createFile(pathAndFileName, newContent);
+	const newContent = tools.convertLinesToStringBlock(newLines);
+	tools.createFile(pathAndFileName, newContent);
+};
+
+export const convertLinesToStringBlock = (lines) => {
+	let r = '';
+	let index = 0;
+	for (const line of lines) {
+		r += line;
+		if (index != lines.length - 1) {
+			r += '\n';
+		}
+		index++;
+	}
+	return r;
+};
+
+export const convertStringBlockToLines = (stringBlock, trimLines = true) => {
+	let roughLines = [];
+
+	if (tools.isEmpty(stringBlock)) {
+		return [];
+	}
+	roughLines = stringBlock.split('\n');
+	if (trimLines) {
+		roughLines = tools.trimAllLinesInLinesArray(roughLines);
+	}
+	roughLines = tools.removeEmptyLinesFromLinesAtBeginningAndEnd(roughLines);
+	return roughLines;
+};
+
+export const trimAllLinesInLinesArray = (lines) => {
+	const newLines = [];
+	lines.forEach(function (line) {
+		let newLine = line.trim();
+		newLines.push(newLine);
+	});
+	return newLines;
+};
+
+export const removeEmptyLinesFromLinesAtBeginningAndEnd = (lines) => {
+	lines = tools.trimAllLinesInLinesArray(lines);
+	lines = tools.removeBlankLinesFromBeginning(lines);
+	lines = lines.reverse();
+	lines = tools.removeBlankLinesFromBeginning(lines);
+	lines = lines.reverse();
+	return lines;
+};
+
+export const removeBlankLinesFromBeginning = (lines) => {
+	const newLines = [];
+	let trimmingBlanks = true;
+	lines.forEach(function (line) {
+		let newLine = line;
+		if (trimmingBlanks && line == "") {
+			//skip it since it is a preceding blank item
+		} else {
+			newLines.push(newLine);
+			trimmingBlanks = false;
+		}
+	});
+	return newLines;
 };
