928122c44c1f56cfe3d4dd29f5d3034fee9ca614
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-17 00:43:00 +0200

    testing cli page create

--- backend/cp.js
+++ backend/cp.js
@@ -6,5 +6,13 @@ if (!pageName) {
 	console.log('EXAMPLE: npm run cp Reports');
 	process.exit();
 } else {
-	console.log(`building page "${pageName}"...`);
+
+	// create code file
+	qfil.createFile(`./src/examples/Page${pageName}.ts`, `export const Page${pageName} = () => {
+	return /*html*/\`
+<div class="page page${pageName}">
+	<p>This is the ${pageName} page.</p>
+</div>
+\`; 
+}`);
 }
--- backend/tools.js
+++ backend/tools.js
@@ -1,3 +1,23 @@
-export const getTestMessage = () => {
-	return 'tools works';
-}
+export const createFile = (pathAndFileName, content) => {
+	fs.writeFileSync(pathAndFileName, content.trim());
+};
+
+export const addLineInFile = (pathAndFileName, marker, additionalLine) => {
+	const content = fs.readFileSync(pathAndFileName, { encoding: 'utf8' });
+	const lines = qstr.convertStringBlockToLines(content);
+	let newLines = [];
+	let lineNumber = 1;
+	for (const line of lines) {
+		if (lineNumber === 1 && marker === '@@FIRSTLINE') {
+			newLines.push(additionalLine);
+		}
+		const newLine = line;
+		newLines.push(newLine);
+		if (newLine.includes(marker)) {
+			newLines.push(additionalLine);
+		}
+		lineNumber++;
+	}
+	const newContent = qstr.convertLinesToStringBlock(newLines);
+	qfil.createFile(pathAndFileName, newContent);
+};
