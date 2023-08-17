82a3ea8d4b71a4d1f9e04981891725ab0ff60065
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-17 02:46:12 +0200

    intro try

--- backend/cp.js
+++ backend/cp.js
@@ -5,6 +5,8 @@ let pageName = process.argv[2];
 
 if (!pageName) {
 	pageName = readlineSync.question('Page name to create (e.g. "About"): ');
+	let introLine = `This is the ${pageName} page.`;
+	introLine = readlineSync.question('Intro line on page: ');
 	createPage(pageName);
 } else {
 	createPage(pageName)
