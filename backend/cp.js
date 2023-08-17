3613a645ebe790d65195fb3719960450445abf3f
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-17 02:34:40 +0200

    about

--- backend/cp.js
+++ backend/cp.js
@@ -4,7 +4,7 @@ import readlineSync from 'readline-sync';
 let pageName = process.argv[2];
 
 if (!pageName) {
-	pageName = readlineSync.question('Page name to create: ');
+	pageName = readlineSync.question('Page name to create (e.g. "About"): ');
 	createPage(pageName);
 } else {
 	createPage(pageName)
