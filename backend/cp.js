9e7ee8140ab33a0d85503624fb36d0eec24f4043
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-17 02:51:36 +0200

    with const

--- backend/cp.js
+++ backend/cp.js
@@ -5,7 +5,7 @@ let pageName = process.argv[2];
 
 if (!pageName) {
 	pageName = readlineSync.question('Page name to create (e.g. "About"): ');
-	customIntroLine = readlineSync.question(`Custom intro line on page (press ENTER for default text): `);
+	const customIntroLine = readlineSync.question(`Custom intro line on page (press ENTER for default text): `);
 	createPage(pageName, customIntroLine);
 } else {
 	createPage(pageName)
